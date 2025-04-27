9fc144941b521bdf26dc61282e65233b04bf3fd1
Ashutosh JHa <ashutoshj.cs.21@nitj.ac.in>
2025-04-27 23:01:38 +0530

    initialised with proper template

---
+++ backend/src/main/java/com/spacesync/spacesync_backend/SpacesyncBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spacesync.spacesync_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpacesyncBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpacesyncBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/spacesync/spacesync_backend/SpacesyncBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spacesync.spacesync_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpacesyncBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
